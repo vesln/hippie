efed5be33e9ea15232eb0b76106e73212000bfd6
Veselin Todorov <veselin@veselin.bg>
2013-11-20 23:17:45 +0200

    Give the ability to end tests when supplying the HTTP method

--- test/get.test.js
+++ test/get.test.js
@@ -8,4 +8,13 @@ describe('#get', function() {
       done();
     });
   });
+
+  it('can perform the test without calling `end`', function(done) {
+    api()
+    .get('http://localhost:7891/get', function(err, res) {
+      should.not.exist(err);
+      res.body.should.eq('GET');
+      done();
+    });
+  });
 });
