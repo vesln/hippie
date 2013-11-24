66f371c46842a5f85f9870ad6df0c4c89b5e4241
Veselin Todorov <veselin@veselin.bg>
2013-11-24 17:21:33 +0200

    Add support for async serializers

--- test/serializer.test.js
+++ test/serializer.test.js
@@ -2,8 +2,8 @@ describe('#serializer', function() {
   it('configures the request body serializer', function(done) {
     api()
     .json()
-    .serializer(function(data) {
-      return JSON.stringify(data);
+    .serializer(function(data, fn) {
+      fn(null, JSON.stringify(data));
     })
     .send({ foo: 'bar' })
     .post('/send-json', done);
