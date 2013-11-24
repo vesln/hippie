8224597b3c6bc01d0a9d14e78c5655b867800208
Veselin Todorov <veselin@veselin.bg>
2013-11-24 17:56:30 +0200

    Add status code expectation

---
+++ test/status-code.test.js
@@ -0,0 +1,16 @@
+describe('#expectStatusCode', function() {
+  it('return an error when the expected status code does not match the actual', function(done) {
+    api()
+    .expectStatusCode(201)
+    .get('/method', function(err) {
+      err.should.be.ok;
+      done();
+    });
+  });
+
+  it('has aliases', function() {
+    var client = api();
+    client.expectStatusCode.should.eq(client.expectStatus);
+    client.expectStatusCode.should.eq(client.expectCode);
+  });
+});
