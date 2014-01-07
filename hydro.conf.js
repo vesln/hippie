156e0aedbaeb413eadf4acd0b95f364eb87e846e
Veselin Todorov <veselin@veselin.bg>
2014-01-07 14:40:48 +0200

    Run the tests with hydro

---
+++ hydro.conf.js
@@ -0,0 +1,54 @@
+/**
+ * Local dependencies.
+ */
+
+var server = require('./test/support/server');
+var hippie = require('./');
+
+/**
+ * Hippie factory.
+ *
+ * Return a new hippie instance with the base URL
+ * configured to the local test server.
+ *
+ * @returns {Hippie}
+ * @api public
+ */
+
+function api() {
+  return hippie().base('http://localhost:' + server.PORT);
+}
+
+/**
+ * Test setup.
+ *
+ * @param {Hydro} hydro
+ * @api public
+ */
+
+module.exports = function(hydro) {
+  hydro.set({
+    suite: 'hippie',
+    formatter: 'hydro-dot',
+    chai: {
+      styles: 'should',
+      showDiff: true,
+      includeStack: true
+    },
+    globals: {
+      api: api
+    },
+    plugins: [
+      'hydro-clean-stacks',
+      'hydro-bdd',
+      'hydro-chai',
+    ],
+    tests: [
+      'test/*.test.js'
+    ]
+  });
+
+  hydro.on('pre:all', function(_, done) {
+    server.listen(server.PORT, done);
+  });
+};
